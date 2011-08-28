29b7f5569ba094d650a7d32fdeb7f81807382332
Andy Brody <git@abrody.com>
2011-08-28 05:31:56 -0400

    Working towards a really working 1.0 version.
    - Add a basic .gitignore.
    - Add a decent launcher icon.
    - Fix brightness setting to actually change brightness immediately.
    - Require android >=2.2.

--- src/com/abrody/brightness/BrightnessToggle.java
+++ src/com/abrody/brightness/BrightnessToggle.java
@@ -1,57 +1,94 @@
 package com.abrody.brightness;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
-import android.provider.Settings.SettingNotFoundException;
-import android.provider.Settings.System; 
+import android.provider.Settings.System;
+import android.view.WindowManager.LayoutParams;
 
 public class BrightnessToggle extends Activity {
-    float BackLightValue = 0.5f; // default value
-    boolean turnoff;
+    private static final int MINIMUM_BACKLIGHT = 18;
+    private static final int MAXIMUM_BACKLIGHT = 255;
+    boolean toManual;
+    
+    // logging constants
+    final static String TAG = "BrightnessToggle";
+    final static boolean LOGV = false;
+    
+    private Handler handler = new Handler();
     
     /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        Log.d("BrightnessToggle", "Clicked!");
-        finish();
         toggleAutoBrightness();
-
+        
+        if (toManual) {
+            // Stall for a bit so the brightness change will actually take effect, then exit.
+            handler.postDelayed(new Runnable() {
+                public void run() { finish(); }
+            }, 300);
+        } else {
+            // Otherwise exit immediately.
+            finish();
+        }
     }
     
     private void toggleAutoBrightness() {
-        int curLevel, newLevel;
-        try {
-            curLevel = System.getInt(getContentResolver(), System.SCREEN_BRIGHTNESS_MODE);
-        } catch (SettingNotFoundException e) {
-            Toast.makeText(this, "Exception!", Toast.LENGTH_SHORT).show();
-            Log.e("BrightnessToggle", "Fail");
-            return;
-        }
+        int mode;
+        int manualBrightnessLevel, brightness;
+        final ContentResolver resolver = getContentResolver();
+
+        // Get the current brightness mode.
+        mode = System.getInt(resolver, System.SCREEN_BRIGHTNESS_MODE, -1);
         
-        turnoff = false;
-        if (curLevel == System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
-            turnoff = true;
-            newLevel = System.SCREEN_BRIGHTNESS_MODE_MANUAL;
-            Toast.makeText(this, "Auto brightness off!", Toast.LENGTH_SHORT).show();
-            Log.i("BrightnessToggle", "Setting SCREEN_BRIGHTNESS_MODE_MANUAL");
-        } else if (curLevel == System.SCREEN_BRIGHTNESS_MODE_MANUAL) {
-            newLevel = System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
-            Toast.makeText(this, "Auto brightness on!", Toast.LENGTH_SHORT).show();
-            Log.i("BrightnessToggle", "Setting SCREEN_BRIGHTNESS_MODE_AUTOMATIC");
+        // Figure out which mode to switch to and pop up a toast.
+        toManual = false;
+        if (mode == System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
+            toManual = true;
+            mode = System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+            Log.i("BrightnessToggle", "Switching to manual");
+            Toast.makeText(this, getString(R.string.toast_off), Toast.LENGTH_SHORT).show();
+        } else if (mode == System.SCREEN_BRIGHTNESS_MODE_MANUAL) {
+            mode = System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
+            Log.i("BrightnessToggle", "Switching to automatic");
+            Toast.makeText(this, getString(R.string.toast_on), Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "Unknown level!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Unknown mode: " + Integer.toString(mode), Toast.LENGTH_SHORT).show();
+            Log.e("BrightnessToggle", "Unknown value for SCREEN_BRIGHTNESS_MODE: " + Integer.toString(mode));
             return;
         }
         
-        if (turnoff) {
-            System.putInt(getContentResolver(), System.SCREEN_BRIGHTNESS, 1);
-            Log.i("BrightnessToggle", "Setting SCREEN_BRIGHTNESS to 1");
-        }
+        // Set the new SCREEN_BRIGHTNESS_MODE
+        System.putInt(resolver, System.SCREEN_BRIGHTNESS_MODE, mode);
         
-        System.putInt(getContentResolver(), System.SCREEN_BRIGHTNESS_MODE, newLevel);
+        // Restore preferences: saved manual brightness
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        manualBrightnessLevel = settings.getInt("manualBrightnessLevel", -1);
+        
+        if (toManual && manualBrightnessLevel >= MINIMUM_BACKLIGHT) {
+            // If we're going to manual, restore the saved brightness level.
+            if (LOGV) Log.d("BrightnessToggle", "Setting SCREEN_BRIGHTNESS to " + Integer.toString(manualBrightnessLevel));
+            System.putInt(resolver, System.SCREEN_BRIGHTNESS, manualBrightnessLevel);
+            
+            // And set the brightness of the window manager
+            LayoutParams attrs = getWindow().getAttributes();
+            attrs.screenBrightness = (float) manualBrightnessLevel / MAXIMUM_BACKLIGHT;
+            if (LOGV) Log.d("BrightnessToggle", "Setting current brightness to " + Float.toString(attrs.screenBrightness));
+            
+        } else {
+            // If we're going to automatic, save current brightness level as default manual level.
+            SharedPreferences.Editor editor = settings.edit();
+            brightness = System.getInt(resolver, System.SCREEN_BRIGHTNESS, -1);
+            
+            if (brightness >= MINIMUM_BACKLIGHT) {
+                editor.putInt("manualBrightnessLevel", brightness);
+                editor.commit();
+            }
+        }
     }
 }
