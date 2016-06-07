4357656c25b0231d644f9596199ecb2281809f2e
Andy Brody <git@abrody.com>
2016-06-06 23:08:26 -0400

    Fix permissions for Android 6 (Marshmallow).
    Also tweak handling of minimum brightness to be a little more forgiving.

--- app/src/main/java/com/abrody/brightness/BrightnessToggle.java
+++ app/src/main/java/com/abrody/brightness/BrightnessToggle.java
@@ -1,40 +1,86 @@
 package com.abrody.brightness;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 import android.provider.Settings.System;
 import android.view.WindowManager.LayoutParams;
 
 public class BrightnessToggle extends Activity {
-    private static final int MINIMUM_BACKLIGHT = 18;
+    private static final int MINIMUM_BACKLIGHT = 5;
     private static final int MAXIMUM_BACKLIGHT = 255;
     boolean toManual;
     
     // logging constants
     final static String TAG = "BrightnessToggle";
-    final static boolean LOGV = false;
-    
+
     private Handler handler = new Handler();
     
     /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate()");
+
         super.onCreate(savedInstanceState);
-        toggleAutoBrightness();
-        
-        if (toManual) {
-            // Stall for a bit so the brightness change will actually take effect, then exit.
-            handler.postDelayed(new Runnable() {
-                public void run() { finish(); }
-            }, 300);
+
+        if (checkForPermissions()) {
+            toggleAutoBrightness();
+
+            if (toManual) {
+                // Stall for a bit so the brightness change will actually take effect, then exit.
+                handler.postDelayed(new Runnable() {
+                    public void run() { finish(); }
+                }, 300);
+            } else {
+                // Otherwise exit immediately.
+                finish();
+            }
         } else {
-            // Otherwise exit immediately.
+            Log.d(TAG, "exiting onCreate()");
             finish();
+        }
+    }
+
+    final private int PERMISSIONS_REQUEST_ID = 42;
+
+    private boolean checkForPermissions() {
+        Log.d(TAG, "checkForPermissions()");
+
+        /*
+         * For some reason, WRITE_SETTINGS is handled via an entirely different method than the
+         * normal "dangerous" settings. We have to start the ACTION_MANAGE_WRITE_SETTINGS activity
+         * to ask the user to manually give us access.
+         */
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.System.canWrite(this)) {
+                return true;
+            } else {
+                Log.d(TAG, "starting action_manage_write_settings activity");
+
+                // start up settings to ask for permission
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + this.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+                Toast.makeText(this, getString(R.string.need_perms), Toast.LENGTH_LONG).show();
+                return false;
+            }
+        } else {
+            return true;
         }
     }
     
@@ -42,6 +88,8 @@ public class BrightnessToggle extends Activity {
         int mode;
         int manualBrightnessLevel, brightness;
         final ContentResolver resolver = getContentResolver();
+
+        Log.v(TAG, "toggleAutoBrightness()");
 
         // Get the current brightness mode.
         mode = System.getInt(resolver, System.SCREEN_BRIGHTNESS_MODE, -1);
@@ -51,16 +99,15 @@ public class BrightnessToggle extends Activity {
         if (mode == System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
             toManual = true;
             mode = System.SCREEN_BRIGHTNESS_MODE_MANUAL;
-            Log.i("BrightnessToggle", "Switching to manual");
+            Log.i(TAG, "Switching to manual");
             Toast.makeText(this, getString(R.string.toast_off), Toast.LENGTH_SHORT).show();
         } else if (mode == System.SCREEN_BRIGHTNESS_MODE_MANUAL) {
             mode = System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
-            Log.i("BrightnessToggle", "Switching to automatic");
+            Log.i(TAG, "Switching to automatic");
             Toast.makeText(this, getString(R.string.toast_on), Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "Unknown mode: " + Integer.toString(mode), Toast.LENGTH_SHORT).show();
-            Log.e("BrightnessToggle", "Unknown value for SCREEN_BRIGHTNESS_MODE: " + Integer.toString(mode));
-            return;
+            throw new UnsupportedOperationException("Unknown value for SCREEN_BRIGHTNESS_MODE: " + mode);
         }
         
         // Set the new SCREEN_BRIGHTNESS_MODE
@@ -70,25 +117,34 @@ public class BrightnessToggle extends Activity {
         SharedPreferences settings = getPreferences(MODE_PRIVATE);
         manualBrightnessLevel = settings.getInt("manualBrightnessLevel", -1);
         
-        if (toManual && manualBrightnessLevel >= MINIMUM_BACKLIGHT) {
+        if (toManual) {
+            if (manualBrightnessLevel < MINIMUM_BACKLIGHT) {
+                Log.v(TAG, "truncating to minimum backlight");
+                manualBrightnessLevel = MINIMUM_BACKLIGHT;
+            }
+
             // If we're going to manual, restore the saved brightness level.
-            if (LOGV) Log.d("BrightnessToggle", "Setting SCREEN_BRIGHTNESS to " + Integer.toString(manualBrightnessLevel));
+            Log.v(TAG, "Setting SCREEN_BRIGHTNESS to " + Integer.toString(manualBrightnessLevel));
             System.putInt(resolver, System.SCREEN_BRIGHTNESS, manualBrightnessLevel);
-            
+
             // And set the brightness of the window manager
             LayoutParams attrs = getWindow().getAttributes();
             attrs.screenBrightness = (float) manualBrightnessLevel / MAXIMUM_BACKLIGHT;
-            if (LOGV) Log.d("BrightnessToggle", "Setting current brightness to " + Float.toString(attrs.screenBrightness));
-            
+            Log.v(TAG, "Setting current brightness to " + Float.toString(attrs.screenBrightness));
+
         } else {
             // If we're going to automatic, save current brightness level as default manual level.
             SharedPreferences.Editor editor = settings.edit();
             brightness = System.getInt(resolver, System.SCREEN_BRIGHTNESS, -1);
-            
-            if (brightness >= MINIMUM_BACKLIGHT) {
-                editor.putInt("manualBrightnessLevel", brightness);
-                editor.commit();
+
+            if (brightness < MINIMUM_BACKLIGHT) {
+                Log.v(TAG, "truncating to minimum backlight before saving");
+                brightness = MINIMUM_BACKLIGHT;
             }
+
+            Log.v(TAG, String.format("Saving manual brightness level as %d", brightness));
+            editor.putInt("manualBrightnessLevel", brightness);
+            editor.commit();
         }
     }
 }
